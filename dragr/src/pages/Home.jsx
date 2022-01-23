d785929e88f7584895e633980c8666bd28fca082
Manny Munoz <em66@uw.edu>
2022-01-23 02:23:37 -0800

    Added fake data for now, added useEffect/useState hook for future firestore queries

--- dragr/src/pages/Home.jsx
+++ dragr/src/pages/Home.jsx
@@ -1,7 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import EventCard from '../components/EventCard';
+import { collection, getDocs } from "firebase/firestore"; 
+import db from '../firebase';
 
 export default function Home() {
+  const [events, setEvents] = useState([]);
+  const getEvents = async () => {
+    //const querySnapshot = await getDocs(collection(db, "events"));
+    setEvents([
+      {
+        performer: "John Doe",
+        location: "New York, NY",
+        date: "Thursday, Jan 27 2022",
+        time: "9PM",
+        picture: "https://source.unsplash.com/400x200/"
+      },
+      {
+        performer: "Jane Doe",
+        location: "Los Angeles, CA",
+        date: "Friday, Jan 28 2022",
+        time: "9PM",
+        picture: "https://source.unsplash.com/400x200/"
+      },
+      {
+        performer: "John Doe",
+        location: "New York, NY",
+        date: "Saturday, Jan 29 2022",
+        time: "9PM",
+        picture: "https://source.unsplash.com/400x200/"
+      },
+      {
+        performer: "Jane Doe",
+        location: "New York, NY",
+        date: "Sunday, Jan 30 2022",
+        time: "9PM",
+        picture: "https://source.unsplash.com/400x200/"
+      }
+    ]);
+  };
+
+  useEffect(() => {
+    getEvents();
+  }, []);
+
+  const renderEvents = () => {
+    return events.map(({performer, location, date, time, picture}) =>
+      <EventCard performer={performer} location={location} date={date} time={time} picture={picture} />
+    );
+  }
+
   const styles = {
     page: {
       display: 'flex',
@@ -19,10 +66,7 @@ export default function Home() {
   return (
     <div>
       <div style={styles.eventsWrapper}>
-        <EventCard performer="John Doe" location="New York, NY" date="Thursday, Jan 27 2022" time="9pm" />
-        <EventCard performer="Jane Doe" location="Los Angeles, CA" date="Friday, Jan 28 2022" time="7pm" />
-        <EventCard performer="John Doe" location="New York, NY" date="Thursday, Jan 27 2022" time="9pm" />
-        <EventCard performer="Jane Doe" location="Los Angeles, CA" date="Friday, Jan 28 2022" time="7pm" />
+        { renderEvents() }
       </div>
     </div>
   );
