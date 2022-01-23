634b05f97706097b86b1bd80ec1523959b7c15d0
Corbin Jensen <corbinjensen@users.noreply.github.com>
2022-01-22 16:18:24 -0800

    Added the start of an events page

---
+++ dragr/src/pages/EventPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+
+export default function EventPage() {
+
+    return (
+        <div>
+          <Header />
+          <div style={styles.eventsWrapper}>
+            <h2>Event Page</h2>
+          </div>
+        </div>
+      );
+}
